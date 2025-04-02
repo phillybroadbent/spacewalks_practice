4c243d0c7c8d4a076ea7ce2d44333ec37a7f2f34
Philippa Broadbent <phillybroadbent@gmail.com>
2025-04-02 14:52:30 +0100

    Add docstrings

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -4,27 +4,50 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+def read_json_to_dataframe(input_file):
+    """
+    Read the data from a JSON file into a pandas dataframe.
+    Clean the data by removing any incomplete rows and sort by date
+    
+    Args:
+        input_file (str): The path to the JSON file
+        
+    Returns:
+        eva_df (pd.DataFrame): The cleaned and sorted data as a dataframe structure
+    """
+    # Read the data from a JSON file into a pandas dataframe
+    eva_df = pd.read_json(input_file, convert_dates=['date'])
+    eva_df['eva'] = eva_df['eva'].astype(float)
+
+    # Clean the data by removing any incomplete rows and sort by date
+    eva_df.dropna(axis=0, inplace=True)
+    eva_df.sort_values('date', inplace=True)
+    return eva_df
+
+def write_dataframe_to_csv(df, output_file):
+    # Save dataframe to CSV for later analysis
+    df.to_csv(output_file, index=False)
+
+# Main code
+
 # https://data.nasa.gov/resource/eva.json (with modifications)
 input_file = open('./eva-data.json', 'r', encoding="utf-8")
 output_file = open('./eva-data.csv','w', encoding="utf-8")
 graph_file = './cumulative_eva_graph.png'  
 
-# Read the data from a JSON file into a pandas dataframe
-eva_df = pd.read_json(input_file, convert_dates=['date'])
-eva_df['eva'] = eva_df['eva'].astype(float)
 
-# Clean the data by removing any incomplete rows and sort by date
-eva_df.dropna(axis=0, inplace=True)
-eva_df.sort_values('date', inplace=True)
+#Read data from JSON file
+eva_data = read_json_to_dataframe(input_file)
 
-# Save dataframe to CSV for later analysis
-eva_df.to_csv(output_file, index=False)
+#Convert and export data to csv file
+write_dataframe_to_csv(eva_data, output_file)
 
-eva_df['duration_hours'] = eva_df['duration'].str.plit(":").apply(lambda x: int(x[0]) + int(x[1]/60))
-eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
+eva_data['duration_hours'] = eva_data['duration'].str.plit(":").apply(lambda x: int(x[0]) + int(x[1]/60))
+eva_data['cumulative_time'] = eva_data['duration_hours'].cumsum()
 
 # Plot cumulative time spent in space over the years
-plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-')
+plt.plot(eva_data['date'], eva_data['cumulative_time'], 'ko-')
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
 plt.tight_layout()
